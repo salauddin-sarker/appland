b5e1bc511c4c573c05ef3a26c91bdc23f06e9125
devsalauddin <mdsalauddinremon@gmail.com>
2023-01-04 17:55:28 +0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('.brand').slick({
+    
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    infinate: true,
+    pauseOnHover:false,
+    speed: 3000,
+    slidesToShow: 4,
+    slidesToScroll: 2
+  })
+  
